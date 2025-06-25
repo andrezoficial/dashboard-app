765d977953d45e7ec86cb327157ba4ff91443bb1
Andres suarez moreno <114126047+andrezoficial@users.noreply.github.com>
2025-06-25 01:24:19 -0500

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { useAuth } from "./AuthContext"; // importa tu contexto de auth
+import { useAuth } from "../context/AuthContext"; // importa tu contexto de auth
 import { useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
