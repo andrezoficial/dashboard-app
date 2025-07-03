c59605d1d84064a94e43fd7d5583e9dbf3570c48
andrezoficial <andres1997rez@gmail.com>
2025-07-02 22:23:59 -0500

    se arregla bugs de citas

--- src/services/api.js
+++ src/services/api.js
@@ -22,3 +22,29 @@ export const eliminarUsuario = async (id) => {
   const res = await axios.delete(`${API_URL}/usuarios/${id}`);
   return res.data;
 };
+// Citas
+export const getCitas = async () => {
+  const res = await axios.get(`${API_URL}/citas`);
+  return res.data;
+};
+
+export const crearCita = async (data) => {
+  const res = await axios.post(`${API_URL}/citas`, data);
+  return res.data;
+};
+
+export const actualizarCita = async (id, data) => {
+  const res = await axios.put(`${API_URL}/citas/${id}`, data);
+  return res.data;
+};
+
+export const eliminarCita = async (id) => {
+  const res = await axios.delete(`${API_URL}/citas/${id}`);
+  return res.data;
+};
+
+// Pacientes
+export const getPacientes = async () => {
+  const res = await axios.get(`${API_URL}/pacientes`);
+  return res.data;
+};
